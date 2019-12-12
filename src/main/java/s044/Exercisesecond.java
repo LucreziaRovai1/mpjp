b347c9c5fa9e4a83c0246b05430660747987c758
Lucrezia Rovai <lucrezia.rovai@hotmail.it>
2019-12-12 12:18:35 +0100

    new esercitazioni

--- src/main/java/s044/Exercisesecond.java
+++ src/main/java/s044/Exercisesecond.java
@@ -4,15 +4,20 @@ public class Exercisesecond {
 	public static void main(String[] args) {
 		int[] array = { 7, 3, 1, 2, 4, 8 };
 		int counter = 0;
-
+		int sum = 0;
+//
 		for (int i = 0; i < array.length; i++) {
-			if (array[i]%2 == 0) {
+			if (array[i] % 2 == 0) {
 				++counter;
+			} else {
+				sum += array[i];
 			}
-		}
 
-		System.out.println(counter+1);
+			 
+		}
+		System.out.println(sum);
+		System.out.println(counter);
+		
 
 	}
-
 }
--- src/main/java/s047/ParameterPassing.java
+++ src/main/java/s047/ParameterPassing.java
@@ -3,75 +3,95 @@ package s047;
 import java.util.Objects;
 
 public class ParameterPassing {
-    static void primitive(int parameter) {
-        parameter += 1;
-        System.out.println("parameter is " + parameter);
-    }
+	static void primitive(int parameter) {
+		parameter += 1;
+		System.out.println("parameter is " + parameter);
+	}
 
-    static void immutableReference(String parameter) {
-        if (parameter == null) {
-            System.out.println("Nothing to do here");
-            return;
-        }
+	static int square(int value) {
+		value *= value;
+		System.out.println("value is " + value);
+		return value;
+	}
 
-        // alternatively: if parameter is null throws a NullPointerException
-        Objects.requireNonNull(parameter, "Parameter should not be null");
+	static int triple(int value) {
+		value *= 3;
+		System.out.println("value is " + value);
+		return value;
+	}
 
-        System.out.println("parameter id was " + System.identityHashCode(parameter));
-        parameter += " there";
-        System.out.println("Now parameter id is " + System.identityHashCode(parameter));
-        System.out.println("parameter value is " + parameter);
-    }
+	static void immutableReference(String parameter) {
+		if (parameter == null) {
+			System.out.println("Nothing to do here");
+			return;
+		}
 
-    static void reference(StringBuilder parameter) {
-        if (parameter == null) {
-            System.out.println("Nothing to do here");
-            return;
-        }
+		// alternatively: if parameter is null throws a NullPointerException
+		Objects.requireNonNull(parameter, "Parameter should not be null");
 
-        // alternatively: if parameter is null throws a NullPointerException
-        Objects.requireNonNull(parameter, "Parameter should not be null");
+		System.out.println("parameter id was " + System.identityHashCode(parameter));
+		parameter += " there";
+		System.out.println("Now parameter id is " + System.identityHashCode(parameter));
+		System.out.println("parameter value is " + parameter);
+	}
 
-        parameter.append(" there");
-        System.out.println("parameter value is " + parameter);
-    }
+	static void reference(StringBuilder parameter) {
+		if (parameter == null) {
+			System.out.println("Nothing to do here");
+			return;
+		}
 
-    static void reference(int[] data) {
-        if (data == null || data.length == 0) {
-            System.out.println("Nothing to do here");
-            return;
-        }
+		// alternatively: if parameter is null throws a NullPointerException
+		Objects.requireNonNull(parameter, "Parameter should not be null");
 
-        // alternatively: if parameter is null throws a NullPointerException
-        Objects.requireNonNull(data, "Parameter should not be null");
+		parameter.append(" there");
+		System.out.println("parameter value is " + parameter);
+	}
 
-        data[0] += 1;
-        System.out.println("data[0] is " + data[0]);
-    }
+	static void reference(int[] data) {
+		if (data == null | data.length == 0) {
+			System.out.println("Nothing to do here");
+			return;
+		}
 
-    public static void main(String[] args) {
-        int primitiveValue = 12;
+		// alternatively: if parameter is null throws a NullPointerException
+		Objects.requireNonNull(data, "Parameter should not be null");
 
-        System.out.println("primitiveValue has been initialized to " + primitiveValue);
-        primitive(primitiveValue);
-        System.out.println("primitiveValue is still " + primitiveValue);
+		data[0] += 1;
+		System.out.println("data[0] is " + data[0]);
+	}
 
-        String string = "Hi";
+	public static void main(String[] args) {
+		int primitiveValue = 12;
 
-        System.out.println("string has been initialized to " + string);
-        immutableReference(string);
-        System.out.println("string is still " + string);
+		System.out.println("primitiveValue has been initialized to " + primitiveValue);
+		primitive(primitiveValue);
 
-        StringBuilder referenceValue = new StringBuilder("Hi");
+		int result = square(primitiveValue);
 
-        System.out.println("referenceValue has been initialized to " + referenceValue);
-        reference(referenceValue);
-        System.out.println("referenceValue now is " + referenceValue);
+		System.out.println("primitiveValue squared is " + result);
 
-        int array[] = { 42 };
+		result = triple(primitiveValue);
+		System.out.println("primitiveValue tripled is " + result);
 
-        System.out.println("array[0] is " + array[0]);
-        reference(array);
-        System.out.println("array[0] now is " + array[0]);
-    }
+		System.out.println("primitiveValue is still " + primitiveValue);
+
+		String string = "Hi";
+
+		System.out.println("string has been initialized to " + string);
+		immutableReference(string);
+		System.out.println("string is still " + string);
+
+		StringBuilder referenceValue = new StringBuilder("Hi");
+
+		System.out.println("referenceValue has been initialized to " + referenceValue);
+		reference(referenceValue);
+		System.out.println("referenceValue now is " + referenceValue);
+
+		int[] array = { 42 };
+
+		System.out.println("array[0] is " + array[0]);
+		reference(array);
+		System.out.println("array[0] now is " + array[0]);
+	}
 }
